2536362dab1b74756a88035846dc33f01a311dfe
TejasDand <tejasdand07@gmail.com>
2025-05-20 19:43:51 +0530

    Weather App Completed

---
+++ Weather-App/main.py
@@ -0,0 +1,27 @@
+# Weather App
+import requests
+import json
+import pyttsx3
+
+print("\nWelcome to the Weather App! (for exit type 'Exit')\n")
+
+while True:
+    city = input("Enter the name of the city: ").title()
+    
+    if city == 'Exit':
+        break
+
+    url = f"https://api.weatherapi.com/v1/current.json?key=d4521167e05f4dd4adf52607252005&q={city}"
+    r = requests.get(url)
+    engine = pyttsx3.init()
+
+    wdic = json.loads(r.text)
+    w = wdic["current"]["temp_c"]
+    updated = wdic["current"]["last_updated"]
+    wind_speed = wdic["current"]["wind_kph"]
+
+    print(f"The current weather in {city} is {w} degrees Celcius and wind speed is {wind_speed} kilometers per hour. Last updated at {updated}.\n")
+    
+    engine.setProperty('rate', 150)
+    engine.say(f"The current weather in {city} is {w} degrees Celcius and wind speed is {wind_speed} kilometers per hour. Last updated at {updated}.")
+    engine.runAndWait()
